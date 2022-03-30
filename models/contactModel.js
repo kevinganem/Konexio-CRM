dd0269c6716d3e8dab1fe1b9b9378e9e4429699e
Kevin GANEM <Ganemkevin@gmail.com>
2022-03-30 10:26:54 +0200

    Schema + routes register and login

---
+++ models/contactModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema({
+  userId: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+  name: String,
+  email: String,
+  description: String,
+  category: Number,
+});
+
+const Contact = mongoose.model("Contact", ContactSchema);
+
+module.exports = Contact;
--- server.js
+++ server.js
@@ -2,15 +2,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+// AUTH
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 // MODELS
+
+// SECRET
+const secret = "5aJif0OZjepB63NRwyNSkk0czzttHKjXNQbEImrW";
 
 // MIDDLEWARES
 app.use(express.json());
+app.use(cookieParser());
 
 // MOMNGODB
 mongoose
   .connect(
-    "mongodb+srv://Konexio-root-kevinganem:root@cluster0.18asb.mongodb.net/Konexio?retryWrites=true&w=majority",
+    "mongodb+srv://Konexio-root-kevinganem:root@cluster0.18asb.mongodb.net/crmDB?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
     }
@@ -19,7 +27,57 @@ mongoose
     console.log("Connected to MongoDB");
   });
 
-// ROUTES
+// ROUTE-REGISTER
+app.post("/register", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+  console.log(hashedPassword);
+
+  try {
+    await User.create({
+      email: req.body.email,
+      password: hashedPassword,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "This account already exists",
+    });
+  }
+
+  res.status(201).json({
+    message: `User ${req.body.email} created`,
+  });
+});
+
+// ROUTE-LOGIN
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  const token = jwt.sign({ id: user._id }, secret);
+
+  res.cookie("jwt", token, { httpOnly: true, secure: false });
+
+  res.json({
+    message: "Here is your cookie",
+  });
+});
+// ROUTE-CONTACT
 
 // START SERVER
 app.listen(8000, () => {
